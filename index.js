0c440ca2ea59d31e05b5243659a425ef6f9d2251
leonardo-santoz <leonardo.cienciacomputer@gmail.com>
2021-03-04 23:28:31 -0300

    graphql yoga: implement. o servidor e 1a query

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
+const { GraphQLServer } = require('graphql-yoga')
 const { prisma } = require('./generated/prisma-client')
 
-async function main() {
-    await prisma.createUser({
-        name: 'Leonardo Santos',
-        email: 'leonardo@gmail.com',
-        password: '123456'
-    })
-
-    const users = await prisma.users()
-
-    console.log('users', users)
+const resolvers = {
+    Query: {
+        user(parent, args, context, info) {
+            return prisma.user({ id: args.id })
+        }
+    }
 }
 
-main().catch(e => console.error(e))
+const server = new GraphQLServer({
+    typeDefs: './schema.graphql',
+    resolvers
+})
+
+server.start().then(() => console.log('Server running on http://localhost:4000...'))
